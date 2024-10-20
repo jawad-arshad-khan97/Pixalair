b6284fa978e912dbaa70782bad2abcf66f0d01f2
JawadKhan <arshad.jak97@gmail.com>
2024-10-20 16:20:44 +0530

    add chnages

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -17,9 +17,6 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    console.log("Connected to mongodb");
-  }
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
@@ -32,10 +29,6 @@ export const connectToDatabase = async () => {
     });
 
   cached.conn = await cached.promise;
-
-  if (cached.conn) {
-    console.log("Connected to mongodb");
-  }
 
   return cached.conn;
 };
