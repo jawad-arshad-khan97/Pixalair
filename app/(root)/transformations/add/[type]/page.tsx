d176ba5ee597c9100d5c9af00655f89b74c945b6
JawadKhan <arshad.jak97@gmail.com>
2024-10-22 20:53:32 +0530

    add changes

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
 
 import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
@@ -19,14 +20,14 @@ const AddTransformationTypePage = async ({
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-      {/* <section className="mt-10"> */}
-      {/* <TransformationForm
+      <section className="mt-10">
+        <TransformationForm
           action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
           creditBalance={user.creditBalance}
-        /> */}
-      {/* </section> */}
+        />
+      </section>
     </>
   );
 };
