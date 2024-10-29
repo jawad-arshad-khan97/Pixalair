10529ec715336b39c4decba835ccf322ce362282
JawadKhan <arshad.jak97@gmail.com>
2024-10-29 18:18:10 +0530

    search cloudinary

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -30,7 +30,7 @@ export const Search = () => {
 
         router.push(newUrl, { scroll: false });
       }
-    }, 300);
+    }, 500);
 
     return () => clearTimeout(delayDebounceFn);
   }, [router, searchParams, query]);
